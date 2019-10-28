b4a9e940ff3ba79b61df58e2274f8bc3f9e6b0c7
yori <47884591+yoritin@users.noreply.github.com>
2019-10-28 09:20:58 +0900

    postをパーシャル化

--- sns_laravel_project/resources/views/posts/index.blade.php
+++ sns_laravel_project/resources/views/posts/index.blade.php
@@ -6,22 +6,7 @@
         <div class="col-md-8">
             <ul>
                 @foreach($posts as $post)
-                <li class="post mb-3">
-                    <div class="user-icon">
-                        <img class="user-image" src="/storage/user_noimage.jpg" alt="noimage" width="40" height="40">
-                    </div>
-                    <div class="post-content">
-                        <div class="post-header">
-                            <a href="{{ action('UsersController@show', $post->user_id) }}">{{ $post->user->name }}</a>
-                        </div>
-                        <div class="post-body">{!! nl2br(e($post->content)) !!}</div>
-                        <div class="post-footer">
-                            <div class="footer-icon"><i class="far fa-heart"></i></div>
-                            <div class="footer-icon"><i class="far fa-comment"></i></div>
-                            <div class="footer-time">{{ $post->updated_at }}</div>
-                        </div>
-                    </div>
-                </li>
+                @include('layouts.post')
                 @endforeach
             </ul>
         </div>
--- sns_laravel_project/resources/views/users/show.blade.php
+++ sns_laravel_project/resources/views/users/show.blade.php
@@ -12,26 +12,10 @@
                     <!-- それぞれのユーザーのユーザーのpost -->
 
                 @foreach($user->posts as $post)
-                <li class="post mb-3">
-                    <div class="user-icon">
-                        <img class="user-image" src="/storage/user_noimage.jpg" alt="noimage" width="40" height="40">
-                    </div>
-                    <div class="post-content">
-                        <div class="post-header">
-                            {{ $post->user->name }}
-                        </div>
-                        <div class="post-body">{!! nl2br(e($post->content)) !!}</div>
-                        <div class="post-footer">
-                            <div class="footer-icon"><i class="far fa-heart"></i></div>
-                            <div class="footer-icon"><i class="far fa-comment"></i></div>
-                            <div class="footer-time">{{ $post->updated_at }}</div>
-                        </div>
-                    </div>
-                </li>
+                @include('layouts.post')
                 @endforeach
             </ul>
         </div>
     </div>
 </div>
-
 @endsection
