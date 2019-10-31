5ab91b6a976196584680bc9a7c89028c60a4c36d
yori <47884591+yoritin@users.noreply.github.com>
2019-10-31 15:46:43 +0900

    マイページとログアウトの位置を入れ替えた

--- sns_laravel_project/resources/views/layouts/app.blade.php
+++ sns_laravel_project/resources/views/layouts/app.blade.php
@@ -55,12 +55,13 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/users', Auth::id()) }}">
+                                        {{ __('my page') }}
+                                    </a>
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
-                                    <a class="dropdown-item" href="{{ url('/users', Auth::id()) }}">
-                                        {{ __('my page') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
