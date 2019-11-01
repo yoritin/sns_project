2eae189fdb54bb106c41fc56335c29cf70b97e97
yori <47884591+yoritin@users.noreply.github.com>
2019-11-01 17:13:57 +0900

    リレーションコントローラーのストアアクションを実装

--- sns_laravel_project/app/Http/Controllers/RelationshipsController.php
+++ sns_laravel_project/app/Http/Controllers/RelationshipsController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\relationship;
 
 class RelationshipsController extends Controller
 {
-    //
+    public function store(Request $request) {
+        $relationship = New Relationship();
+        $relationship->user_id = $request->user_id;
+        $relationship->followed_user_id = $request->followed_user_id;
+        $relationship->save();
+        return redirect()->back();
+    }
 }
