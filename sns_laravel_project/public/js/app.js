bd897261419fb189fb68ca33235694ff3b2a2135
yori <47884591+yoritin@users.noreply.github.com>
2019-11-15 13:40:57 +0900

    JQueryでpostのコメント部分が全部開いてしまう問題を解決

--- sns_laravel_project/public/js/app.js
+++ sns_laravel_project/public/js/app.js
@@ -49382,15 +49382,9 @@ var app = new Vue({
 /***/ (function(module, exports) {
 
 $(function () {
-  // .comment-showをクリックしたら　data-idに格納された$post->idを　コンソールに出力する
   $('.comment-show').click(function () {
-    var post_id = $('.comment-show').data('id');
-    console.log(post_id);
-  }); // コンソールを確認すると一番上の$post->idしか取得できない
-
-  $('.comment-show').click(function () {
-    // ここでif文を使用して取得したpost_idとクリックした場所の$post->idを比べて一致したら、以下の処理をする
-    $('.post-footer-comment').toggle('slow');
+    // クリックした要素から動作させたい要素までを指定
+    $(this).parent('.footer-icon').parent('.post-footer').next('.post-footer-comment').toggle('slow');
   });
 });
 
