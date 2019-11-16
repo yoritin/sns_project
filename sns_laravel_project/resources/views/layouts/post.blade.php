52a91bb78c31aaa77fcabe5542bc1570fafe2446
yori <47884591+yoritin@users.noreply.github.com>
2019-11-16 14:18:19 +0900

    ログインユーザー以外はいいね機能を使用できない

--- sns_laravel_project/resources/views/layouts/post.blade.php
+++ sns_laravel_project/resources/views/layouts/post.blade.php
@@ -29,19 +29,22 @@
         <div class="post-body">{!! nl2br(e($post->content)) !!}</div>
         <div class="post-footer">
             <div class="footer-icon">
-                <!-- {{ \App\Like::where('user_id', Auth::id())->first()['user_id'] }} -->
-                @if(Auth::id() === \App\Like::where('user_id', Auth::id())->where('post_id', $post->id)->first()['user_id'])
-                <i class="fas fa-heart red"></i>
+                @guest
+                <i class="far fa-heart"></i>
                 {{ \App\Like::where('post_id', $post->id)->count() }}
                 @else
-                <form method="post" action="{{ url('/likes') }}">
-                    @csrf
-                    <input type="hidden" value="{{ $post->id }}" name="post_id">
-                    <button type="submit"><i class="far fa-heart"></i></button>
+                    @if(Auth::id() === \App\Like::where('user_id', Auth::id())->where('post_id', $post->id)->first()['user_id'])
+                    <i class="fas fa-heart red"></i>
                     {{ \App\Like::where('post_id', $post->id)->count() }}
-                </form>
-                @endif
-
+                    @else
+                    <form method="post" action="{{ url('/likes') }}">
+                        @csrf
+                        <input type="hidden" value="{{ $post->id }}" name="post_id">
+                        <button type="submit"><i class="far fa-heart"></i></button>
+                        {{ \App\Like::where('post_id', $post->id)->count() }}
+                    </form>
+                    @endif
+                @endguest
             </div>
             <div class="footer-icon">
                 <i class="far fa-comment comment-show" data-id="{{ $post->id }}"></i>
