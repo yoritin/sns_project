b5c703285280d4f6217036054c643879756da917
yori <47884591+yoritin@users.noreply.github.com>
2019-11-17 15:23:02 +0900

    Confirm Password機能を実装

--- sns_laravel_project/app/Http/Controllers/UsersController.php
+++ sns_laravel_project/app/Http/Controllers/UsersController.php
@@ -23,9 +23,18 @@ class UsersController extends Controller
     }
 
     public function update(Request $request, User $user) {
+        $this->validate($request, [
+            'name' => 'required',
+            'email' => 'required',
+            'password' => 'required|confirmed|min:8',
+            'password_confirmation' => 'required',
+        ]);    
         $user->name = $request->name;
         $user->email = $request->email;
         $user->password = Hash::make($request['password']);
+
+
+
         $auth = Auth::id();
         $user->save();
         return view('users.show')->with('user', $user);
