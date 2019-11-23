15e423a506a61a3d1f0ee03d6c7e2c77b9ac8635
yori <47884591+yoritin@users.noreply.github.com>
2019-11-23 18:36:40 +0900

    ログアウトが機能しなくなってしまった不具合を修正

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -40,6 +40,9 @@
                                         document.getElementById('logout-form').submit();">
                             {{ __('ログアウト') }}
                         </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </li>
                 @endguest
             </ul>
