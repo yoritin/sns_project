117ad9fd7609b88ec2a05fce16b22f93bc7ac0d9
yori <47884591+yoritin@users.noreply.github.com>
2019-11-23 17:32:20 +0900

    navbarをパーシャル化

---
+++ resources/views/layouts/navbar.blade.php
@@ -0,0 +1,66 @@
+<nav class="navbar navbar-expand-md navbar-dark bg-primary shadow-sm">
+    <div class="container">
+        <a class="navbar-brand" href="{{ url('/') }}">
+            {{ config('app.name', 'sns_project') }}
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                    </li>
+                    @if (Route::has('register'))
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                        </li>
+                    @endif
+                @else
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/posts/new-info') }}">
+                            {{ __('新着') }}
+                        </a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            {{ Auth::user()->name }} <span class="caret"></span>
+                        </a>
+                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{ url('/users', Auth::id()) }}">
+                                {{ __('マイページ') }}
+                            </a>
+                            @if (Auth::id() === 1)
+                            <a class="dropdown-item" href="">
+                                {{ __('設定出来ません') }}
+                            </a>
+                            @else
+                            <a class="dropdown-item" href="{{ action('UsersController@edit', Auth::id()) }}">
+                                {{ __('設定') }}
+                            </a>
+                            @endif
+                            <a class="dropdown-item" href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();">
+                                {{ __('ログアウト') }}
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                @csrf
+                            </form>
+                        </div>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="{{ url('/posts/create') }}">
+                            <i class="fas fa-edit"></i>
+                        </a>
+                    </li>
+                @endguest
+            </ul>
+        </div>
+    </div>
+</nav>
