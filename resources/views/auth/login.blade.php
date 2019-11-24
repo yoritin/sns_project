2c08fb2f34b4ffb2006e921d645daf156b629276
yori <47884591+yoritin@users.noreply.github.com>
2019-11-24 15:38:02 +0900

    login画面のUIを変更

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,51 +4,48 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <form method="POST" action="{{ route('login') }}">
+            <form method="POST" action="{{ route('login') }}" class="row">
                 @csrf
-                
-                <div class="form-group row">
-                    
-                    <div class="col-md-6 offset-md-3">
-                        <h2>{{ __('ログイン') }}</h2>
+                <div class="form-border col-md-6 offset-md-3">
+                    <p class="my-3 form-title">{{ __('ログイン') }}</p>
+                    <div class="form-group">
                         <label for="email" class="col-form-label text-md-right">{{ __('メールアドレス') }}</label>
                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
                         @error('email')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
+                    </div>
+                    <div class="form-group">
                         <label for="password" class="col-form-label text-md-right">{{ __('パスワード') }}</label>
                         <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
+        
                         @error('password')
                             <span class="invalid-feedback" role="alert">
                                 <strong>{{ $message }}</strong>
                             </span>
                         @enderror
                     </div>
-                </div>
-                <div class="form-group row">
-                    <div class="col-md-6 offset-md-3">
+                    <div class="form-group">
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
+        
                             <label class="form-check-label" for="remember">
                                 {{ __('ログインを記憶') }}
                             </label>
                         </div>
                     </div>
-                </div>
-
-                <div class="form-group row mb-0">
-                    <div class="col-md-8 offset-md-3">
+                    <div class="form-group">
                         <button type="submit" class="btn btn-primary">
                             {{ __('ログイン') }}
                         </button>
                         <div>
                             <a href="{{ url('/register') }}">アカウントをお持ちでない方はこちら</a>
                         </div>
+                    </div>
+                    <div class="form-group">
+                        
                     </div>
                 </div>
             </form>
